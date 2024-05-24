0c80f8d3270c16348d9556c2ff00ab5d6ccd13a4
Stiware <stiwar-quintero@hotmail.com>
2024-05-24 11:50:28 -0500

    Change the reviews model to have more information about ir

--- model/Review.js
+++ model/Review.js
@@ -1,17 +1,26 @@
 const mongoose = require('mongoose')
     
 const songReviews = new mongoose.Schema({
-    song_name: String,
-    artist: String,
-    album: String,
-    album_cover: String,
-    genre: String,
-    year: Number,
-    rating: Number,
-    review: String
+    user: {
+        userName: String,
+        userId: String,
+        userImage: String
+    },
+    song_details:{
+        song_name: String,
+        artist: String,
+        album: String,
+        album_cover: String,
+        genre: String,
+        year: Number,
+        rating: Number,
+        review: String
+    }
 })
 
+
 module.exports = mongoose.model("review", songReviews)
+
 
 
 // await SR.findById('664e04d280ca255d96eb2428').then((data) => console.log(data))
